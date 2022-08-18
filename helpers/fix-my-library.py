3ce5c5927e364d5cb659f0b485d9592334865caa
Jos <me@josfeenstra.nl>
2022-08-18 17:27:59 +0200

    I hate you sometimes latex

--- helpers/fix-my-library.py
+++ helpers/fix-my-library.py
@@ -1,15 +1,16 @@
 # I'm using Zotero for generating a bibtex, but that produces corrupt bibtex's sometimes, or in any case: 
 # Latex doesnt like them. This fix works. I think the abstract & file can contain special characters, confusing latex.
 
-# I dont know how to do streaming, so this scripts reads everything in memory 
+# I dont know how to do streaming with python, 
+# so this scripts reads everything in memory 
 
 # old files 
 # file = './P2/proposal/My Library.bib'
 # fileNew = './P2/proposal/library.bib'
 
 # P4 library adress
-file = "./P4/thesis/My Library.bib"
-fileNew = "./P4/thesis/library.bib"
+file = "./latex/My Library.bib"
+fileNew = "./latex/library.bib"
 
 # read
 with open(file, 'r', encoding='utf-8') as file:
@@ -17,8 +18,10 @@ with open(file, 'r', encoding='utf-8') as file:
 
 # process
 for i, line in enumerate(data):
+    # remove all abstracts
     if 'abstract = ' in line: 
         data[i] = ''
+    # remove all attached files
     if 'file = ' in line: 
         data[i] = ''
 
